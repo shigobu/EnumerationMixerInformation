e03a21137ecf74ec2f8022fd1e3c6ae2f3836aab
kasamatsu <kasamatsu@kasa.com>
2021-03-17 10:56:47 +0900

    ウィンドウズ標準の音量ミキサーの情報を取得するようにした。

--- MixerInformation/Program.cs
+++ MixerInformation/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using NAudio.CoreAudioApi;
 
 namespace MixerInformation
 {
@@ -10,6 +12,52 @@ namespace MixerInformation
     {
         static void Main(string[] args)
         {
+            MMDevice device = null;
+            try
+            {
+                //ウマ娘セッションの検索
+                using (MMDeviceEnumerator DevEnum = new MMDeviceEnumerator())
+                {
+                    device = DevEnum.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
+                }
+                //master情報
+                Console.WriteLine("FriendlyName:" + device.FriendlyName);
+                Console.WriteLine("DeviceFriendlyName:" + device.DeviceFriendlyName);
+                Console.WriteLine("IconPath:" + Environment.ExpandEnvironmentVariables(device.IconPath));
+                Console.WriteLine("");
+
+                AudioSessionManager sessionManager = device.AudioSessionManager;
+                var sessions = sessionManager.Sessions;
+                for (int j = 0; j < sessions.Count; j++)
+                {
+                    Console.WriteLine("DisplayName:" + sessions[j].DisplayName);
+                    Console.WriteLine("IconPath:" + sessions[j].IconPath);
+                    Console.WriteLine("");
+                }
+            }
+            finally
+            {
+                if (device != null)
+                {
+                    device.Dispose();
+                }
+            }
+
+            Console.Read();
+
         }
+    }
+
+    class NativeMethods
+    {
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern bool FreeLibrary(IntPtr hModule);
+
+        [DllImport("kernel32", SetLastError = true, CharSet = CharSet.Auto)]
+        static extern IntPtr LoadLibrary(string lpFileName);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        static extern int LoadString(IntPtr hInstance, uint uID, StringBuilder lpBuffer, int nBufferMax);
     }
 }
